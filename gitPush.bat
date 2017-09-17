19c92844ff69b224415a4f55c85b7a6300bda737
mozi <mozi.song@outlook.com>
2017-09-17 17:16:36 +0800

    update chapter notes

--- gitPush.bat
+++ gitPush.bat
@@ -1,7 +1,5 @@
 #!/bin/sh
-#start "" "E:\Program Files\Git\git-bash.exe" --login -i -c "git add ." "git commit -m "update chapter notes"" "git push"
 git add .
 git commit -m "update chapter notes"
 git push
-ECHO Done.
-PAUSE
+ECHO done.
